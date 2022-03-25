90816811c15086f733d9168f3f953c368d9498d6
Ben Baert <benbaert246@gmail.com>
2022-03-25 15:59:34 +0100

    Explicitly set project id

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 echo "${FIREBASE_SERVICE_ACCOUNT}" | base64 --decode > "$PROJECT_DIR"/serviceaccount.json
 export GOOGLE_APPLICATION_CREDENTIALS=PROJECT_DIR/serviceaccount.json
 
+firebase use "$PROJECT_ID"
+
 firebase deploy --only firestore:rules
