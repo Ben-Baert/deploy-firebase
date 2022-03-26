a7916fdca0e2bb5d26bcbab7e80478ed48db9f83
Ben Baert <benbaert246@gmail.com>
2022-03-26 12:36:47 +0100

    Use GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "${FIREBASE_SERVICE_ACCOUNT}" | base64 --decode > "$PROJECT_DIR"/serviceaccount.json
-export GOOGLE_APPLICATION_CREDENTIALS=PROJECT_DIR/serviceaccount.json
+echo "${FIREBASE_SERVICE_ACCOUNT}" | base64 --decode > "$GITHUB_WORKSPACE"/serviceaccount.json
+export GOOGLE_APPLICATION_CREDENTIALS=$GITHUB_WORKSPACE/serviceaccount.json
 
 firebase use "$PROJECT_ID"
 
