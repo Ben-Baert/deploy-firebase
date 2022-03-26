4a6b78df77be2fcb7c6ffb786bdd3f54623e1d5a
Ben Baert <benbaert246@gmail.com>
2022-03-26 12:51:27 +0100

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,13 @@
 set -e   # exit on first failed command
 set -x   # print all executed commands to the log
 
+
+
 echo "${FIREBASE_SERVICE_ACCOUNT}" | base64 --decode > "$GITHUB_WORKSPACE"/serviceaccount.json
 export GOOGLE_APPLICATION_CREDENTIALS=$GITHUB_WORKSPACE/serviceaccount.json
 
-firebase use "$PROJECT_ID"
+echo "Done."
 
-firebase deploy --only firestore:rules
+#firebase use "$PROJECT_ID"
+
+#firebase deploy --only firestore:rules
