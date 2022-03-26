cc3caf9ae6b6ec3aab59f14030684d5c2d4252a2
Ben Baert <benbaert246@gmail.com>
2022-03-26 12:46:35 +0100

    -e -x flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e   # exit on first failed command
+set -x   # print all executed commands to the log
+
 echo "${FIREBASE_SERVICE_ACCOUNT}" | base64 --decode > "$GITHUB_WORKSPACE"/serviceaccount.json
 export GOOGLE_APPLICATION_CREDENTIALS=$GITHUB_WORKSPACE/serviceaccount.json
 
